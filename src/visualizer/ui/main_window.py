e6815768acbd3833c553924654f09b50b0e58a14
Inforable <hasri.fayadh@gmail.com>
2025-07-21 16:40:57 +0700

    feat: add initial window for GUI and add requirements.txt file

---
+++ src/visualizer/ui/main_window.py
@@ -0,0 +1,8 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Quaternion Visualizer")
+        self.resize(1280, 720)
